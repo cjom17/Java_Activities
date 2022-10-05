141223cf291af91bc8764f4ab434104af6b39bfb
cjom17 <nztechnophile.1@gmail.com>
2022-10-05 20:10:03 +0800

    added java files

---
+++ EXCEPTIONS/JavaExceptions.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class JavaExceptions{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("How many objects to create: ? ");
+
+        // to be added
+
+        
+    }
+}
---
+++ EXCEPTIONS/SportsCars.java
@@ -0,0 +1,14 @@
+public class SportsCars {
+    private String name;
+    private String feature;
+    private int price;
+
+    public void setname(String name){
+        this.name = name;
+    }
+    public String getname(){
+        return name;
+    }
+
+    
+}
